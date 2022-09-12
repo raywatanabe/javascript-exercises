4e33f4ff9a3a084bb4d06fda48eb582d5a3b6fae
raywatanabe <raywatanabe23@gmail.com>
2022-09-11 22:29:42 -0600

    Edit reverseString.js and reverseString.spec.js
    Removed skips from spec.js
    Implemented reverseString function to convert given string to an array, reverse the array
    convert it back into a string, then remove the commas

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    let reverseArray = array.reverse();
+    let newString = reverseArray.toString();
+    newString = newString.replaceAll(",", "");
+    return newString;
 };
 
 // Do not edit below this line
