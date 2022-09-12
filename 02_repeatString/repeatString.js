2c7e9d86ca6733d432f8c93de935bfb41b323910
raywatanabe <raywatanabe23@gmail.com>
2022-09-11 21:19:48 -0600

    Edit repeatString.js and repeatString.spec.js
    Made repeatString function repeat the given string for a specified amount of times
    using .repeat method.
    Made repeatString function return "ERROR" for negative numbers.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+let newString = "";
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        newString = string.repeat(num);
+        return newString;
+    } else {
+        return "ERROR";
+    }
+    
 };
-
+console.log(newString);
 // Do not edit below this line
 module.exports = repeatString;
