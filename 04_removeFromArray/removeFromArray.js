7aa9d437baeef3d4f8c9ec1d8a02ecf34d8eff04
raywatanabe <raywatanabe23@gmail.com>
2022-09-13 17:31:16 -0600

    Edit removeFromArray.js removeFromArray.spec.js
    Removed skips from spec.js
    Implemented removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    // First argument will be array
+    let array = arguments[0];
 
+    // Starting from second argument, check to see if argument is present in array. If yes, then remove from array
+    for (let j = 1; j < arguments.length; j++) {
+        for (let i = 0; i < array.length; i++) {       
+            if (arguments[j] === array[i]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
