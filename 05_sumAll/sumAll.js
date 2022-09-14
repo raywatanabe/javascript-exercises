cf572a8a0e68415a9adce1c9436e03922a17abab
raywatanabe <raywatanabe23@gmail.com>
2022-09-14 11:00:36 -0600

    Edit sumAll.js sumAll.spec.js
    Removed skips from spec.js
    Made sumAll function to loop from num1 to num2 (lower to higher)
    sumAll adds each number in loop to sum and returns it
    sumAll returns "ERROR" for non-number parameters or numbers less than 0

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else if (typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
+    else if (num1 < num2) {
+        for (let i = num1, n = num2; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else if (num1 > num2) {
+        for (let i = num2, n = num1; i <=n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
